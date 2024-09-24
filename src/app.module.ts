db544a20385f58d39fc7448d7b2db7d6011b956f
Frank Licona (3FE 3LE) <3l33f3@gmail.com>
2024-09-24 18:00:30 -0500

    feat: :sparkles: add initials resources rooms & reservations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RoomsModule } from './rooms/rooms.module';
+import { ReservationsModule } from './reservations/reservations.module';
 
 @Module({
-  imports: [],
+  imports: [RoomsModule, ReservationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
