75633f43b703e31ab86ca80fc9c751e5f1457dc0
Frank Licona (3FE 3LE) <3l33f3@gmail.com>
2024-09-25 16:04:18 -0500

    docs: :memo: add swagger, config genaration of openapi file to import documentation & config CORS

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,45 @@
+import * as fs from 'fs';
+
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const globalPrefix = 'api';
+  app.setGlobalPrefix(globalPrefix);
+
+  const appConfig = new DocumentBuilder()
+    .setTitle('Stellar Backend API')
+    .setDescription('Stellar Backend API documentation')
+    .setVersion('1.0')
+    .build();
+  const appDocument = SwaggerModule.createDocument(app, appConfig, {
+    ignoreGlobalPrefix: false,
+  });
+  fs.writeFileSync('openapi.json', JSON.stringify(appDocument, null, 2));
+  SwaggerModule.setup(`${globalPrefix}/`, app, appDocument);
+
+  const allowedOrigins = ['http://localhost:3001'];
+
+  app.enableCors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  });
+
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+  );
 }
 bootstrap();
